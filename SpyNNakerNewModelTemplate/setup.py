19b67facdd239679c7ab30dd3b427d264d1607ca
alan-stokes <alan.barry.stokes@gmail.com>
2016-04-05 17:41:21 +0100

    changes for new release

--- SpyNNakerNewModelTemplate/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="REPLACE_WITH_MODULE_NAME",
-    version="REPLACE_WITH_MODULE_VERSION",
-    packages=['python_models',],
-    package_data={'python_models.model_binaries': ['*.aplx']},
-    install_requires=['SpyNNaker == 2015.005-rc-01']
-)
