169a9153d042d699cedd99a5911f2e6287a9cc00
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2016-04-26 12:09:40 +0100

    Updated version

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version="REPLACE_WITH_MODULE_VERSION",
     packages=['python_models',],
     package_data={'python_models.model_binaries': ['*.aplx']},
-    install_requires=['SpyNNaker == 2015.005-rc-01']
+    install_requires=['SpyNNaker == 2016.001']
 )
