fbad21befe18d8a8a5396a5016d5de197f6814db
mbassor2 <mbassor2@cspc457.cs.man.ac.uk>
2017-05-12 11:11:34 +0100

    Update naming convention: synapse_types_combined_exponential_impl.h

--- c_models/src/neuron/synapse_types/synapse_types_combined_exponential_impl.h
+++ c_models/src/neuron/synapse_types/synapse_types_combined_exponential_impl.h
@@ -1,6 +1,6 @@
 /*! \file
- * \brief implementation of synapse_types.h for a synapse behaviour calculated as the difference between two exponential functions - sometimes called an 'alpha' input?
- *
+ * \brief implementation of synapse_types.h for a synapse behaviour
+ *  calculated as the difference between two exponential functions
  */
 
 #ifndef _DIFF_SYNAPSE_H_
@@ -19,16 +19,16 @@
 // Synapse parameters
 //---------------------------------------
 typedef struct synapse_param_t {
-	input_t exc_input_buffer_value;
+	input_t exc_response;
 	decay_t exc_A_decay;
 	decay_t exc_A_init;
-	input_t exc_A_input_buffer_value;
+	input_t exc_A_response;
 	decay_t exc_B_decay;
 	decay_t exc_B_init;
-	input_t exc_B_input_buffer_value;
+	input_t exc_B_response;
 	decay_t inh_decay;
 	decay_t inh_init;
-	input_t inh_input_buffer_value;
+	input_t inh_response;
 } synapse_param_t;
 
 #include <neuron/synapse_types/synapse_types.h>
@@ -39,64 +39,22 @@ typedef enum input_buffer_regions {
 	EXCITATORY, INHIBITORY,
 } input_buffer_regions;
 
-//---------------------------------------
-// Synapse shaping inline implementation
-//---------------------------------------
-
-
-/*
-// Define helper methods here to improve code readability below
-// Offsets
-static inline index_t _ex_offset(index_t neuron_index) {
-	return synapse_types_get_input_buffer_index(EXCITATORY, neuron_index);
-}
-
-static inline index_t _ex_A_offset(index_t neuron_index) {
-	return synapse_types_get_input_buffer_index(EXCITATORY_A, neuron_index);
-}
-
-static inline index_t _ex_B_offset(index_t neuron_index) {
-	return synapse_types_get_input_buffer_index(EXCITATORY_B, neuron_index);
-}
-
-static inline index_t _in_offset(index_t neuron_index) {
-	return synapse_types_get_input_buffer_index(INHIBITORY, neuron_index);
-}
-
-// Decays
-static inline decay_t _ex_A_decay(synapse_param_t *parameters,
-		index_t neuron_index) {
-	return (parameters[neuron_index].exc_A_decay);
-}
-
-static inline decay_t _ex_B_decay(synapse_param_t *parameters,
-		index_t neuron_index) {
-	return (parameters[neuron_index].exc_B_decay);
-}
-
-static inline decay_t _in_decay(synapse_param_t *parameters,
-		index_t neuron_index) {
-	return (parameters[neuron_index].inh_decay);
-}
-
-// Required implementation methods
-*/
 
 static inline void synapse_types_shape_input(synapse_param_pointer_t parameter){
 	//(input_t *input_buffers, index_t neuron_index, synapse_param_t* parameters) {
-	parameter->exc_A_input_buffer_value =  decay_s1615(
-			parameter->exc_A_input_buffer_value,
+	parameter->exc_A_response =  decay_s1615(
+			parameter->exc_A_response,
 			parameter->exc_A_decay);
-	parameter->exc_B_input_buffer_value = decay_s1615(
-			parameter->exc_B_input_buffer_value,
+	parameter->exc_B_response = decay_s1615(
+			parameter->exc_B_response,
 			parameter->exc_B_decay);
 
-	parameter->inh_input_buffer_value = decay_s1615(
-			parameter->inh_input_buffer_value,
+	parameter->inh_response = decay_s1615(
+			parameter->inh_response,
 			parameter->inh_decay);
 
 	//log_info("Decay A value: %11.4k, Decay B value: %11.4k", parameters[neuron_index].exc_B_decay, parameters[neuron_index].exc_A_decay);
-	parameter->exc_input_buffer_value = parameter->exc_B_input_buffer_value - parameter->exc_A_input_buffer_value;
+	parameter->exc_response = parameter->exc_B_response - parameter->exc_A_response;
 
 
 	/* log_info("shaping comb %11.4k, A %11.4k, B %11.4k", input_buffers[_ex_offset(neuron_index)], input_buffers[_ex_B_offset(
@@ -111,12 +69,12 @@ static inline void synapse_types_add_neuron_input(
 
 	if (synapse_type_index == EXCITATORY) {
 
-		parameter->exc_A_input_buffer_value =  parameter->exc_A_input_buffer_value +
+		parameter->exc_A_response =  parameter->exc_A_response +
 				decay_s1615(input,
 				parameter->exc_A_init);
 
 
-		parameter->exc_B_input_buffer_value = parameter->exc_B_input_buffer_value +
+		parameter->exc_B_response = parameter->exc_B_response +
 				decay_s1615(input,
 				parameter->exc_B_init);
 
@@ -124,7 +82,7 @@ static inline void synapse_types_add_neuron_input(
 
 		//log_info("init A value: %11.4k, init B value: %11.4k", parameters[neuron_index].exc_B_init, parameters[neuron_index].exc_A_init);
 
-		parameter->exc_input_buffer_value = parameter->exc_B_input_buffer_value - parameter->exc_A_input_buffer_value;
+		parameter->exc_response = parameter->exc_B_response - parameter->exc_A_response;
 
 
 		//input_buffers[_ex_offset(neuron_index)]= input_buffers[_ex_B_offset(neuron_index)] - input_buffers[_ex_A_offset(neuron_index)];
@@ -134,7 +92,7 @@ static inline void synapse_types_add_neuron_input(
 		*/
 
 	} else if (synapse_type_index == INHIBITORY) {
-		parameter->inh_input_buffer_value = parameter->inh_input_buffer_value +
+		parameter->inh_response = parameter->inh_response +
 				decay_s1615(input,
 				parameter->inh_init);
 	}
@@ -146,12 +104,12 @@ static inline void synapse_types_add_neuron_input(
  */
 static inline input_t synapse_types_get_excitatory_input(
 		synapse_param_pointer_t parameter) {
-	return parameter->exc_input_buffer_value;
+	return parameter->exc_response;
 }
 
 static inline input_t synapse_types_get_inhibitory_input(
 		synapse_param_pointer_t parameter) {
-	return parameter->inh_input_buffer_value;
+	return parameter->inh_response;
 }
 
 static inline const char *synapse_types_get_type_char(
@@ -176,24 +134,24 @@ static inline void synapse_types_print_input(
         synapse_param_pointer_t parameter) {
     io_printf(
         IO_BUF, "%12.6k + %12.6k - %12.6k",
-        parameter->exc_input_buffer_value,
-        parameter->exc_A_input_buffer_value,
-        parameter->exc_B_input_buffer_value,
-        parameter->inh_input_buffer_value);
+        parameter->exc_response,
+        parameter->exc_A_response,
+        parameter->exc_B_response,
+        parameter->inh_response);
 }
 
 static inline void synapse_types_print_parameters(synapse_param_pointer_t parameter) {
     log_debug("-------------------------------------\n");
-	log_debug("exc_input_buffer_value  = %11.4k\n", parameter->exc_input_buffer_value);
+	log_debug("exc_response  = %11.4k\n", parameter->exc_response);
 	log_debug("exc_A_decay  = %11.4k\n", parameter->exc_A_decay);
 	log_debug("exc_A_init   = %11.4k\n", parameter->exc_A_init);
-	log_debug("exc_A_input_buffer_value  = %11.4k\n", parameter->exc_A_input_buffer_value);
+	log_debug("exc_A_response  = %11.4k\n", parameter->exc_A_response);
 	log_debug("exc_B_decay = %11.4k\n", parameter->exc_B_decay);
 	log_debug("exc_B_init  = %11.4k\n", parameter->exc_B_init);
-	log_debug("exc_B_input_buffer_value  = %11.4k\n", parameter->exc_B_input_buffer_value);
+	log_debug("exc_B_response  = %11.4k\n", parameter->exc_B_response);
 	log_debug("inh_decay  = %11.4k\n", parameter->inh_decay);
 	log_debug("inh_init   = %11.4k\n", parameter->inh_init);
-	log_debug("inh_input_buffer_value  = %11.4k\n", parameter->inh_input_buffer_value);
+	log_debug("inh_response  = %11.4k\n", parameter->inh_response);
 }
 
 #endif // _DIFF_SYNAPSE_H_
